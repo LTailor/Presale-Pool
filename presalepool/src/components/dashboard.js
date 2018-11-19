183f416fec50e328bb9c8e7a993588ef9d911083
LTailor <dzkhasayev@gmail.com>
2018-11-19 15:11:20 +0300

    change styles

--- presalepool/src/components/dashboard.js
+++ presalepool/src/components/dashboard.js
@@ -60,7 +60,6 @@ export class DashboardComponent extends React.Component {
 
   onInputValueChange(event) {
     this.setState({[event.target.id]: event.target.value });
-    alert(JSON.stringify(this.state))
   }
 
   render () {
