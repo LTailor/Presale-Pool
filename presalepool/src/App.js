b54da7313661ad92c4e1ec8654baf722c3c646f4
Dzhambulat <dzkhasayev@gmail.com>
2018-11-28 21:06:19 +0300

    add web3 to store

--- presalepool/src/App.js
+++ presalepool/src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import { Header, WalletComponent, CreateComponent, DashboardComponent, AdminComponent } from './components';
 import { Route } from 'react-router-dom';
+import { inject } from "mobx-react";
 import './assets/stylesheets/application.css';
 
+@inject("Stores")
 export class App extends React.Component {
+
+  componentDidMount() {
+    let results
+    let web3 = window.web3
+
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+      // Use Mist/MetaMask's provider.
+      web3 = new window.Web3(web3.currentProvider)
+      web3.version.getNetwork((err, netId) => {
+        var defaultAccount = web3.eth.defaultAccount || null;
+        results = {
+          web3Instance: web3,
+          netId,
+          injectedWeb3: true,
+          defaultAccount
+        }
+
+        this.props.Stores.setWeb3(web3);
+      })
+    }
+  }
+
   render(){
     if(!window.web3) {
       return <div> You must use Metamask</div>
     }
+
     return (
       <div>
         <Header/>
--- presalepool/src/stores/Store.js
+++ presalepool/src/stores/Store.js
@@ -6,6 +6,10 @@ class Store {
     this.walletStore = new WalletStore()
     this.poolSettingsStore = new PoolSettingsStore()
   }
+
+  setWeb3(web3) {
+    this.web3 = web3;
+  }
 }
 
 export default new Store();
